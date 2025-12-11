7791859e45208dbab8ba0186dba088682d786a84
razesoldier <razesoldier@outlook.com>
2025-12-11 18:05:13 +0800

    Follow-up e3f3a8: fix test

--- src/main/kotlin/dev/nohus/rift/Main.kt
+++ src/main/kotlin/dev/nohus/rift/Main.kt
@@ -1,10 +1,6 @@
 package dev.nohus.rift
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ApplicationScope
 import androidx.compose.ui.window.LocalWindowExceptionHandlerFactory
@@ -15,16 +11,16 @@ import dev.nohus.rift.crash.RiftExceptionHandlerFactory
 import dev.nohus.rift.crash.handleFatalException
 import dev.nohus.rift.di.koin
 import dev.nohus.rift.di.startKoin
+import dev.nohus.rift.i18n.ApplicationLocale
 import dev.nohus.rift.logging.initializeLogging
 import dev.nohus.rift.notifications.NotificationsController
-import dev.nohus.rift.settings.persistence.Settings
 import dev.nohus.rift.singleinstance.SingleInstanceWrapper
 import dev.nohus.rift.splash.SplashWindowWrapper
 import dev.nohus.rift.tray.RiftTray
 import dev.nohus.rift.windowing.WindowManager
 import dev.nohus.rift.wizard.WizardWindowWrapper
 import io.kamel.image.config.LocalKamelConfig
-import java.util.Locale
+import java.util.*
 
 fun main() {
     try {
@@ -49,10 +45,10 @@ private fun ApplicationScope.riftApplication() {
         val viewModel: ApplicationViewModel = viewModel()
         val windowManager: WindowManager = remember { koin.get() }
         val notificationsController: NotificationsController = remember { koin.get() }
-        val currentLocale : Locale = koin.get<Settings>(Settings::class).language
+        ApplicationLocale.useSettingsLocale()
         val state by viewModel.state.collectAsState()
 
-        Locale.setDefault(currentLocale)
+        Locale.setDefault(ApplicationLocale.current)
         RiftTheme {
             if (state.isAnotherInstanceDialogShown) {
                 SingleInstanceWrapper(
